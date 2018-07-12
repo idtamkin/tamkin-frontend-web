210cc20b03dc7ec63a212dd0f4e387634266bbcb
Endy Muhardin <endy.muhardin@gmail.com>
2018-07-12 10:06:10 +0700

    install vue-router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App)
